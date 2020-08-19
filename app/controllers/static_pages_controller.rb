ec7db524fef38732911957ad03e720aa21e3487f
JiaChenHuang <jiachen.huang@mvtrail.com>
2020-08-19 13:44:03 +0800

    新建控制器、路由、模块

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,21 @@
+class StaticPagesController < ApplicationController
+
+  
+  # 首页
+  def home
+  end
+
+  # 帮助
+  def help
+  end
+
+  # 关于
+  def about
+  end
+
+  # 联系
+  def contact
+    
+  end
+  
+end
