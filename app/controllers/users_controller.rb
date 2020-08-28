59fe5ad9319b3e85914863806bc753701cadb460
JiaChenHuang <jiachen.huang@mvtrail.com>
2020-08-28 17:39:56 +0800

    实现登录功能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,19 @@ class UsersController < ApplicationController
   # 显示
   def show
     @user = User.find(params[:id])
-    # debugger
   end
-  # Ruby on Rails 247
+# 实现注册
+def create 
+  @user = User.new(user_params)
+  if @user.save
+    flash[:success] = "Welcome to the Sample App"
+    redirect_to  @user
+  else 
+    render "new"
+  end
+end
+private
+def user_params
+         params.require(:user).permit(:name, :email, :password, :password_confirmation)
+end
 end
